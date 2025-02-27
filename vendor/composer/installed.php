8bd77534ebc27ed5cb6adfa21b7fb18a6404716f
Yakov Medvedev <yakovmedvedev@gmail.com>
2025-02-27 19:26:03 +0300

    all the files

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => '1.0.0+no-version-set',
+        'version' => '1.0.0.0',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => NULL,
+        'name' => 'yam/small-talk-with-hexlet',
+        'dev' => true,
+    ),
+    'versions' => array(
+        'yam/small-talk-with-hexlet' => array(
+            'pretty_version' => '1.0.0+no-version-set',
+            'version' => '1.0.0.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => NULL,
+            'dev_requirement' => false,
+        ),
+    ),
+);
